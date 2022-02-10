7e035630e9cf6d785e3dfeab65d587913ef7e1c6
Pouya Shekari <pshekari37@gmail.com>
2022-02-10 21:55:31 +0330

    add products table

--- src/pages/PanelProduct/PanelProduct.Page.js
+++ src/pages/PanelProduct/PanelProduct.Page.js
@@ -1,40 +1,63 @@
-import {Component} from 'react'
-import {Helmet} from 'react-helmet';
-import {connect} from 'react-redux';
-import {fetchProducts} from 'redux/actions/product.action';
+import {useState} from "react"
 import {AdminLayout} from "../../layout";
+import {Helmet} from 'react-helmet'
+import {ProductsTable, ProductModal} from "../../../components/index"
+import {Typography, Button, Grid} from "@material-ui/core"
+import { makeStyles } from '@material-ui/core/styles';
+import productApi from "../../../api/products.api"
 
-class PanelProductPage extends Component {
-    componentDidMount() {
-        try {
-            this.props.getProducts();
-        } catch (e) {
-            console.log('error products');
-        }
+
+const useStyles = makeStyles({
+    container:{
+        margin:'20px auto 0',
+        display: 'flex',
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }
+});
+
+const PanelProduct  = () => {
+    const classes = useStyles();
+    const [modalOpenHandler, setModalOpenHandler] = useState({modalHandler:null})
+    const [modalMode, setModalMode] = useState({mode:null, data:null})
+
+    const openModalButtonHandler = async ()=>{
+        await setModalMode({...modalMode,mode:'add'})
+        modalOpenHandler.modalHandler()
     }
 
-    render() {
-        return (
-            <>
-                <Helmet>
-                    <title>Article | About Us</title>
-                </Helmet>
-
-                <AdminLayout>
-                    <h1>صفحه پنل محصولات</h1>
-                </AdminLayout>
-            </>
-
-        );
+    const openModalEditButtonHandler = async (row)=>{
+        await setModalMode({data:row,mode:'edit'})
+        modalOpenHandler.modalHandler()
     }
+
+    const openModalDeleteButtonHandler = async (row)=>{
+        await setModalMode({...modalMode,mode:'delete'})
+        await productApi.delete(row.id)
+        setModalMode({...modalMode, mode:'default'})
+    }
+
+    return (
+        <>
+            <Helmet>
+                <title>Project | Products Panel</title>
+            </Helmet>
+
+            <AdminLayout>
+                <Grid item lg={8} md={10} sm ={10} xs={10} className={classes.container}  onClick={openModalButtonHandler}>
+                    <Button variant="contained" color="primary" >افزودن کالا</Button>
+                    <Typography variant="h4" component="p">مدیریت کالا ها</Typography>
+                </Grid>
+                <ProductsTable
+                    openModalEditButtonHandler={openModalEditButtonHandler}
+                    openModalDeleteButtonHandler={openModalDeleteButtonHandler}
+                    mode={modalMode.mode}
+                />
+                <ProductModal setModalOpenHandler={setModalOpenHandler} mode={modalMode.mode} product={modalMode.data} setMode={setModalMode}/>
+            </AdminLayout>
+        </>
+    )
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getProducts: () => dispatch(fetchProducts())
-    }
-};
-
-const PanelProduct = connect(undefined, mapDispatchToProps)(PanelProductPage)
-
-export {PanelProduct};
+export {PanelProduct}
